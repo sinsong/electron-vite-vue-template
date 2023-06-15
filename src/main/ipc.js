a042a264dd5d9229572050d3c877ce7f7888699a
sinsong <literalkernel@outlook.com>
2023-06-15 14:33:03 +0800

    Validate the sender of IPC messages
    https://www.electronjs.org/docs/latest/tutorial/security#17-validate-the-sender-of-all-ipc-messages

--- src/main/ipc.js
+++ src/main/ipc.js
@@ -1,7 +1,13 @@
 const { ipcMain } = require('electron/main')
 
+function validateSender (frame) {
+  let url = new URL(frame.url)
+  return url.protocol === 'app:' && url.host === 'renderer'
+}
+
 export default function ipc() {
   ipcMain.handle('ping', (event) => {
+    if (!validateSender(event.senderFrame)) return null
     return 'pong'
   })
 }
